3be86003183c1cc2308ff0021254b5ef58945ee8
Yu YAGIHASHI <yagihash@ssrf.dev>
2026-02-07 00:31:35 +0900

    add test for default key version

--- input/input_test.go
+++ input/input_test.go
@@ -24,6 +24,51 @@ func TestLoad(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	os.Unsetenv("INPUT_APP_ID")
+	os.Unsetenv("INPUT_OWNER")
+	os.Unsetenv("INPUT_REPOSITORIES")
+	os.Unsetenv("INPUT_PERMISSION_CONTENTS")
+	os.Unsetenv("INPUT_PERMISSION_ISSUES")
+	os.Unsetenv("INPUT_BASE_URL")
+	os.Unsetenv("INPUT_KMS_PROJECT_ID")
+	os.Unsetenv("INPUT_KMS_KEYRING_ID")
+	os.Unsetenv("INPUT_KMS_KEY_ID")
+	os.Unsetenv("INPUT_KMS_KEY_VERSION")
+	os.Unsetenv("INPUT_KMS_LOCATION")
+}
+
+func TestLoad_DefaultKeyVersion(t *testing.T) {
+	os.Setenv("INPUT_APP_ID", "12345")
+	os.Setenv("INPUT_OWNER", "owner")
+	os.Setenv("INPUT_REPOSITORIES", "owner/repo1,owner/repo2")
+	os.Setenv("INPUT_PERMISSION_CONTENTS", "write")
+	os.Setenv("INPUT_PERMISSION_ISSUES", "read")
+	os.Setenv("INPUT_BASE_URL", "https://api.github.com")
+	os.Setenv("INPUT_KMS_PROJECT_ID", "project-id")
+	os.Setenv("INPUT_KMS_KEYRING_ID", "keyring-id")
+	os.Setenv("INPUT_KMS_KEY_ID", "key-id")
+	os.Setenv("INPUT_KMS_LOCATION", "us-central1")
+
+	i, err := Load()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if i.KeyVersion != "1" {
+		t.Errorf("KeyVersion should be set to default value: %s", i.KeyVersion)
+	}
+
+	os.Unsetenv("INPUT_APP_ID")
+	os.Unsetenv("INPUT_OWNER")
+	os.Unsetenv("INPUT_REPOSITORIES")
+	os.Unsetenv("INPUT_PERMISSION_CONTENTS")
+	os.Unsetenv("INPUT_PERMISSION_ISSUES")
+	os.Unsetenv("INPUT_BASE_URL")
+	os.Unsetenv("INPUT_KMS_PROJECT_ID")
+	os.Unsetenv("INPUT_KMS_KEYRING_ID")
+	os.Unsetenv("INPUT_KMS_KEY_ID")
+	os.Unsetenv("INPUT_KMS_LOCATION")
 }
 
 func TestRepositories_Decode(t *testing.T) {
